26f1a4d7032f5a87c8e7f58a93eb6e575c3563bf
Korede <solomonkorede15@gmail.com>
2022-09-23 23:05:09 +0100

    sending contact to backend

--- Js/script.js
+++ Js/script.js
@@ -158,3 +158,36 @@ window.addEventListener('load', () => {
     document.querySelector('.preloader').style.display = 'none';
   }, 600);
 });
+
+const contactForm = document.querySelector('.contact-form');
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let subject = document.getElementById('subject');
+let message = document.getElementById('message');
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let formData = {
+    name: name.value,
+    email: email.value,
+    subject: subject.value,
+    message: message.value,
+  };
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', '/');
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.onload = function () {
+    console.log(xhr.responseText);
+    if (xhr.responseText == 'success') {
+      alert('Email Sent To Korede');
+      name.value = '';
+      email.value = '';
+      subject.value = '';
+      message.value;
+    } else {
+      alert('Something happened resend email');
+    }
+  };
+  xhr.send(JSON.stringify(formData));
+});
